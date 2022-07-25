f8416866aa4543a0e41d8b679790dda3b580c8f4
AbdelrahmanElawady <abdoelawady125@gmail.com>
2022-07-25 09:03:41 +0200

    better error handling and embed swagger json file instead of hardcoding it

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 
 	"net/http"
@@ -17,8 +16,6 @@ import (
 
 const DBFILE = "./data/todo.db"
 
-var db, err = gorm.Open(sqlite.Open(DBFILE), &gorm.Config{})
-
 type ToDoData struct {
 	Text string
 }
@@ -32,102 +29,148 @@ func (ToDo) TableName() string {
 	return "ToDo"
 }
 
-func GetAllToDosHandler(w http.ResponseWriter, r *http.Request) {
-	rows, err := db.Model(&ToDo{}).Rows()
+type ErrorResponse struct {
+	Error string
+}
+
+type Server struct {
+	DB *gorm.DB
+}
+
+func (s *Server) GetAllToDosHandler(w http.ResponseWriter, r *http.Request) {
+	rows, err := s.DB.Model(&ToDo{}).Rows()
 
 	if err != nil {
-		panic("error parsing data")
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
+		return
 	}
 	defer rows.Close()
 	for rows.Next() {
 		var res ToDo
-		db.ScanRows(rows, &res)
+		err := s.DB.ScanRows(rows, &res)
+		if err != nil {
+			errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+			http.Error(w, string(errJSON), http.StatusInternalServerError)
+			return
+		}
 		data, err := json.Marshal(res)
 		if err != nil {
-			panic("error parsing data")
+			errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+			http.Error(w, string(errJSON), http.StatusInternalServerError)
+			return
 		}
 		w.Write(data)
 		w.Write([]byte("\n"))
 
 	}
 }
-func AddToDoHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) AddToDoHandler(w http.ResponseWriter, r *http.Request) {
 	var todoData ToDoData
 	err := json.NewDecoder(r.Body).Decode(&todoData)
 	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
 		return
 	}
-	db.Create(&ToDo{Text: todoData.Text})
+	s.DB.Create(&ToDo{Text: todoData.Text})
 	var res ToDo
-	db.Last(&res)
+	s.DB.Last(&res)
 	data, err := json.Marshal(res)
 	if err != nil {
-		panic("error parsing data")
-	}
-	w.Write(data)
-	w.Write([]byte("\n"))
-}
-
-func GetToDoHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	var res ToDo
-	db.First(&res, id)
-	data, err := json.Marshal(res)
-	if err != nil {
-		panic("error parsing data")
-	}
-	w.Write(data)
-	w.Write([]byte("\n"))
-}
-
-func UpdateToDoHandler(w http.ResponseWriter, r *http.Request) {
-	vars := mux.Vars(r)
-	id := vars["id"]
-	var todoData ToDoData
-	err := json.NewDecoder(r.Body).Decode(&todoData)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusBadRequest)
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
 		return
 	}
-	db.Create(&ToDo{Text: todoData.Text})
-	var res ToDo
-	db.Model(&ToDo{}).Where("ID = ?", id).Update("Text", todoData.Text)
-	db.First(&res, id)
-	data, err := json.Marshal(res)
-	if err != nil {
-		panic("error parsing data")
-	}
 	w.Write(data)
 	w.Write([]byte("\n"))
 }
 
-func DeleteToDoHandler(w http.ResponseWriter, r *http.Request) {
+func (s *Server) GetToDoHandler(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	id := vars["id"]
-	result := db.Delete(&ToDo{}, id)
+	var res ToDo
+	result := s.DB.First(&res, id)
 	if result.Error != nil {
-		panic("error deleting data")
+		errJson, _ := json.Marshal(ErrorResponse{Error: result.Error.Error()})
+		http.Error(w, "{\"error\":\""+string(errJson)+"\"}", http.StatusInternalServerError)
+		return
 	}
-	fmt.Fprintf(w, "%q Deleted", id)
+
+	data, err := json.Marshal(res)
+	if err != nil {
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+	w.Write([]byte("\n"))
+}
+
+func (s *Server) UpdateToDoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	var todoData ToDoData
+	err := json.NewDecoder(r.Body).Decode(&todoData)
+	if err != nil {
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
+		return
+	}
+	s.DB.Create(&ToDo{Text: todoData.Text})
+	var res ToDo
+	result := s.DB.Model(&ToDo{}).Where("ID = ?", id).Update("Text", todoData.Text)
+	if result.Error != nil {
+		errJson, _ := json.Marshal(ErrorResponse{Error: result.Error.Error()})
+		http.Error(w, "{\"error\":\""+string(errJson)+"\"}", http.StatusInternalServerError)
+		return
+	}
+	e := s.DB.First(&res, id)
+	if e.Error != nil {
+		errJson, _ := json.Marshal(ErrorResponse{Error: e.Error.Error()})
+		http.Error(w, "{\"error\":\""+string(errJson)+"\"}", http.StatusInternalServerError)
+		return
+	}
+	data, err := json.Marshal(res)
+	if err != nil {
+		errJSON, _ := json.Marshal(ErrorResponse{Error: err.Error()})
+		http.Error(w, string(errJSON), http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+	w.Write([]byte("\n"))
+}
+
+func (s *Server) DeleteToDoHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	result := s.DB.Delete(&ToDo{}, id)
+	if result.Error != nil {
+		errJson, _ := json.Marshal(ErrorResponse{Error: result.Error.Error()})
+		http.Error(w, "{\"error\":\""+string(errJson)+"\"}", http.StatusInternalServerError)
+		return
+	}
+	s.GetAllToDosHandler(w, r)
 
 }
 
 func main() {
+	s := Server{}
+	var err error
+	s.DB, err = gorm.Open(sqlite.Open(DBFILE), &gorm.Config{})
 
 	if err != nil {
 		panic("couldn't connect")
 	}
-	db.AutoMigrate(&ToDo{})
+	s.DB.AutoMigrate(&ToDo{})
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/todo", GetAllToDosHandler).Methods("GET")
-	r.HandleFunc("/todo", AddToDoHandler).Methods("POST")
-	r.HandleFunc("/todo/{id}", GetToDoHandler).Methods("GET")
-	r.HandleFunc("/todo/{id}", UpdateToDoHandler).Methods("PUT")
-	r.HandleFunc("/todo/{id}", DeleteToDoHandler).Methods("DELETE")
+	r.HandleFunc("/todo", s.GetAllToDosHandler).Methods("GET")
+	r.HandleFunc("/todo", s.AddToDoHandler).Methods("POST")
+	r.HandleFunc("/todo/{id}", s.GetToDoHandler).Methods("GET")
+	r.HandleFunc("/todo/{id}", s.UpdateToDoHandler).Methods("PUT")
+	r.HandleFunc("/todo/{id}", s.DeleteToDoHandler).Methods("DELETE")
 	r.PathPrefix("/swagger").Handler(httpSwagger.WrapHandler)
 
 	log.Fatal(http.ListenAndServe(":5000", r))
